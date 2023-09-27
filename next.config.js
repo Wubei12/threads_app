b2358e271a9af65931b2f6375de0830f7739628a
Wubei 12 <wubshetyenguss@gmail.com>
2023-09-27 05:29:45 +0300

    configness

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
@@ -27,10 +30,6 @@ const nextConfig = {
         hostname: "utfs.io",
       },
     ],
-
-    typescript: {
-      ignoreBuildErrors: true,
-    },
   },
 };
 
