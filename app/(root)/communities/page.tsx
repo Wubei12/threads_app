b740de5ab446c01d91233d57ee9611e67d7359aa
Wubei 12 <wubshetyenguss@gmail.com>
2023-09-27 05:13:59 +0300

    initial

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <section>
+            <h1 className="mb-10 head-text">Communities</h1>
+
+        </section>
+    )
+}
+
+export default Page
